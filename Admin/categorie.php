59999fb908ec8b8231212fad13a9faace7291e1d
Mohamed Bouda <126961762+boudaMed75@users.noreply.github.com>
2023-12-22 16:47:40 +0100

    Categories page

---
+++ Admin/categorie.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+    <?php
+        include_once "php/global/head.php";
+    ?>
+<body>
+    <div class="page d-flex">
+        <?php include_once "php/global/sidebar.php" ?>
+        <div class="content w-full">
+            <div class="head bg-white p-15 between-flex">
+            <?php include_once "php/global/header.php" ?>
+            </div>
+            
+            
+            <div class="tache_select">
+                <div data-type="affichage" class="selected">Les Code promo</div>
+                <div data-type="ajouter">Ajouter Nouveau</div>
+            </div>
+
+            <div id="filtre" class="filtre">
+                <select name="" id="">
+                    
+                </select>
+            </div>
+            
+            <div id="project" class="p-20 bg-white rad-10 m-20">
+              
+           
+            
+            </div> 
+</div>
+
+</div>
+
+
+    <!-- scriptjs -->
+    <script src="js/category.js"></script>
+    <script src="js/global.js"></script>
+</body>
+</html>
