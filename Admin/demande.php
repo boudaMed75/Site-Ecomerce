58fdd1f053262363c85d89c7400523ba98830ffd
Mohamed Bouda <126961762+boudaMed75@users.noreply.github.com>
2023-12-22 16:52:18 +0100

    page

---
+++ Admin/demande.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <?php
+        include_once "php/global/head.php";
+    ?>
+<body>
+    <div class="page d-flex">
+        <?php include_once "php/global/sidebar.php" ?>
+        <div class="content w-full">
+            <div class="head bg-white p-15 between-flex">
+            <?php include_once "php/global/header.php" ?>
+            </div>
+            
+        </div>
+    </div>
+</body>
+<script src="js/global.js"></script>
+</html>
