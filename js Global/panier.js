e1c259fbf2c02365bed2e52c2219dd4f2b12aee4
Mohamed Bouda <126961762+boudaMed75@users.noreply.github.com>
2023-12-23 11:07:09 +0100

    Update panier.js

--- js Global/panier.js
+++ js Global/panier.js
@@ -1,1 +1,14 @@
+const ovrir = document.querySelector('.code'),
+code_promo = document.querySelector('.code_promo'),
+btn_add = code_promo.querySelector('button'),
+input_code = document.querySelector('input');
+
+
+// ovrir.onclick = ()=>{
+//     code_promo.classList.remove('d-none');
+//     btn_add.onclick = ()=>{
+//         console.log(input_code.value);
+//     }
+// }
+
 
