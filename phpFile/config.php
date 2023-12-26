82af8f8532954931ced106a09212f6e7920c97bb
Mohamed Bouda <126961762+boudaMed75@users.noreply.github.com>
2023-12-26 21:50:43 +0100

    Update config.php

--- phpFile/config.php
+++ phpFile/config.php
@@ -1,1 +1,9 @@
 
+<?php
+    $con = new PDO("mysql:server=localhost;dbname=ecomerce","root","");
+    $con->query("SET NAMES 'utf8mb4'");
+    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    if(!$con){
+        echo "DataBase Error !";
+    }
+?>
